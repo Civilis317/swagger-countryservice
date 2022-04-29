7820d80c722e07fc7bc9c93d475be01b5a4f330f
Civilis317 <ljwzeilstra@gmail.com>
2022-04-29 19:51:04 +0200

    test commit

--- src/main/java/org/boip/util/countryservice/SwaggerConfig.java
+++ src/main/java/org/boip/util/countryservice/SwaggerConfig.java
@@ -45,6 +45,6 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
-    // push test3
+    // push test4
 
 }
